9df66822d672efbd885066080fa51b0a7cf71b4a
joneqian <qian.qing@aliyun.com>
2015-12-31 17:54:11 +0800

    user address list

--- common/models/address.js
+++ common/models/address.js
@@ -21,5 +21,29 @@ module.exports = function(Address) {
       }
     );
 
+    //获取用户地址
+    Address.getUserAddress = function (pageId, pageSize, cb) {
+      //TODO: cloud logic
+      cb(null, {count:100, data:[
+        {id:1, default:true, province:1, city:1, region:1, road:1, addDetail:'XX小区门口'},
+        {id:2, default:true, province:1, city:1, region:1, road:1, addDetail:'XX商店'}
+      ], msg:''});
+    };
+
+    Address.remoteMethod(
+      'getUserAddress',
+      {
+        description: ['获取用户地址信息(access token),返回结果-count:该用户总的地址数量, data:该次查询的地址数组'],
+        accepts: [
+          {arg: 'pageId', type: 'number', required: true, description: '第几页'},
+          {arg: 'pageSize', type: 'number', required: true, description: '每页记录数'}
+        ],
+        returns: {arg: 'addressInfo', type: 'string'},
+        http: {path: '/get-user-address', verb: 'get'}
+      }
+    );
+
+
+
   });
 };

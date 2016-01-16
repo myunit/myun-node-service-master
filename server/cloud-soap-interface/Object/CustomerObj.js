d2eb5e5d3553f48ac51c8e6cff47fea40ae79f34
joneqian <qian.qing@aliyun.com>
2016-01-16 09:45:09 +0800

    add xml pack

--- server/cloud-soap-interface/Object/CustomerObj.js
+++ server/cloud-soap-interface/Object/CustomerObj.js
@@ -4,6 +4,7 @@
  * @description
  */
 var utils = require('../../util/utils');
+var xml = require('xml');
 
 exports.createRegisterXML = function (regObj) {
   regObj.Name = regObj.Name || '';
@@ -17,19 +18,50 @@ exports.createRegisterXML = function (regObj) {
   regObj.WangwangNo = regObj.WangwangNo || '';
   regObj.invitationCode = regObj.invitationCode || '';
 
-  return '<Register xmlns="http://tempuri.org/">' +
-    '<registerInfo xmlns:d4p1="http://schemas.datacontract.org/2004/07/MYun.BPC.Contract.CustomerMgmt.Data"' +
-    ' xmlns:i="http://www.w3.org/2001/XMLSchema-instance">' +
-    '<d4p1:BirthDay>' + regObj.BirthDay + '</d4p1:BirthDay>' +
-    '<d4p1:CellPhoneNo>' + regObj.CellPhoneNo + '</d4p1:CellPhoneNo>' +
-    '<d4p1:CustomerFrom>' + regObj.CustomerFrom + '</d4p1:CustomerFrom>' +
-    '<d4p1:CustomerLevel>' + regObj.CustomerLevel + '</d4p1:CustomerLevel>' +
-    '<d4p1:CustomerSource>' + regObj.CustomerSource + '</d4p1:CustomerSource>' +
-    '<d4p1:Gender>' + regObj.Gender + '</d4p1:Gender>' +
-    '<d4p1:HeadPicture>' + regObj.HeadPicture + '</d4p1:HeadPicture>' +
-    '<d4p1:LoginPassword>' + regObj.LoginPassword + '</d4p1:LoginPassword>' +
-    '<d4p1:Name>' + regObj.Name + '</d4p1:Name>' +
-    '<d4p1:StoreName>' + regObj.StoreName + '</d4p1:StoreName>' +
-    '<d4p1:WangwangNo>' + regObj.WangwangNo + '</d4p1:WangwangNo>' +
-    '</registerInfo>' + '<invitationCode>' + regObj.invitationCode + '</invitationCode></Register>';
+  var regXML = [{
+    Register: [
+      {
+        _attr: {
+          xmlns: 'http://tempuri.org/'
+        }
+      },
+      {
+        registerInfo: [
+          {
+            _attr: {
+              'xmlns:d4p1': 'http://schemas.datacontract.org/2004/07/MYun.BPC.Contract.CustomerMgmt.Data',
+              'xmlns:i': 'http://www.w3.org/2001/XMLSchema-instance'
+            }
+          }, {
+            'd4p1:BirthDay': regObj.BirthDay
+          }, {
+            'd4p1:CellPhoneNo': regObj.CellPhoneNo
+          }, {
+            'd4p1:CustomerFrom': regObj.CustomerFrom
+          }, {
+            'd4p1:CustomerLevel': regObj.CustomerLevel
+          }, {
+            'd4p1:CustomerSource': regObj.CustomerSource
+          }, {
+            'd4p1:Gender': regObj.Gender
+          }, {
+            'd4p1:HeadPicture': regObj.HeadPicture
+          }, {
+            'd4p1:LoginPassword': regObj.LoginPassword
+          }, {
+            'd4p1:Name': regObj.Name
+          }, {
+            'd4p1:StoreName': regObj.StoreName
+          }, {
+            'd4p1:WangwangNo': regObj.WangwangNo
+          }
+        ]
+      },
+      {
+        invitationCode: regObj.invitationCode
+      }
+    ]
+  }];
+
+  return xml(regXML, true);
 };

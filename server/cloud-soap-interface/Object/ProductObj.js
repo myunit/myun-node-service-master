8037f19a629163cc6828e4c94df9e94852fa87b4
joneqian <qian.qing@aliyun.com>
2016-04-04 15:54:56 +0800

    乡货圈首页

--- server/cloud-soap-interface/Object/ProductObj.js
+++ server/cloud-soap-interface/Object/ProductObj.js
@@ -24,7 +24,7 @@ exports.getAllProductXML = function (obj) {
 
 exports.getAllProductLightXML = function (obj) {
   var xmlObj = [{
-    GetAllGetProduct_lightForApp: [
+    GetAllGetProduct_LightForApp: [
       {
         _attr: {
           xmlns: 'http://tempuri.org/'
--- server/cloud-soap-interface/product-ifs.js
+++ server/cloud-soap-interface/product-ifs.js
@@ -27,9 +27,9 @@ ProductIFS.prototype.getAllProduct = function (obj, callback) {
 ProductIFS.prototype.getAllProductLight = function (obj, callback) {
   var Product = this.DS.models.Product;
   var xml = ProductObj.getAllProductLightXML(obj);
-  Product.GetAllGetProduct_lightForApp(xml, function (err, response) {
+  Product.GetAllGetProduct_LightForApp(xml, function (err, response) {
     try {
-      callback(err, JSON.parse(response.GetAllGetProduct_lightForAppResult));
+      callback(err, JSON.parse(response.GetAllGetProduct_LightForAppResult));
     } catch (e) {
       callback(err, {IsSuccess: false, ErrorDescription:'服务异常'});
     }
